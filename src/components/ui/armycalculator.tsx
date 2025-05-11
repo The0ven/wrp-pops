b6a13696d677f798d16e5bd556883b2a0b130fc1
The0ven <ovencock@gmail.com>
2025-05-11 22:07:10 +0200

    add army calc

---
+++ src/components/ui/armycalculator.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { useState } from "react"
+import { Input } from "./input"
+
+function ArmyCalculator({pop}: {pop: number}) {
+  const [rate, setRate] = useState(0.01)
+  return (
+    <div className="flex gap-4">
+        <Input className="max-w-24" value={rate}  type="number" onChange={(e)=>setRate(parseFloat(e.currentTarget.value))}/>
+        <p className="text-muted-foreground">{rate * 100}%: {(pop * rate).toLocaleString() || '-'}</p>
+    </div>
+  )
+}
+
+export default ArmyCalculator
